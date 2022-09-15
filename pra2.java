2c5fde030e5c5ba4421f4d68bf1a8895a8d2efad
nishiyu <y.nishikawa.sys22@morijyobi.ac.jp>
2022-09-15 10:38:46 +0900

    Hello Worldを
    100回作成するプログラムを
    作成しました、

--- pra2.java
+++ pra2.java
@@ -1,2 +1,9 @@
-Hello World
-
+public static void main(String[] args) {
+        for(int i=0; i<=100; i++){
+            if(i<=100){
+                i++;
+                System.out.println("Hello World");
+                continue;
+            } 
+    }
+}
