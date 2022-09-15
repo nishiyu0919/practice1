c7148379fd49b1a4c3ff4a831223f501a6af09aa
nishiyu <y.nishikawa.sys22@morijyobi.ac.jp>
2022-09-15 12:41:07 +0900

    100回以上
    実行結果を表示する
    プログラムを作りました。

--- pra2.java
+++ pra2.java
@@ -1,6 +1,6 @@
-java2{
+public class java2{
 public static void main(String[] args) {
-        for(int i=0; i<=100; i++){
+        for(int i=0; i<100; i++){
             
                 
                 System.out.println("Hello World");
