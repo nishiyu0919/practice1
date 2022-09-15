eb3f62a3a28eb4c955d5f443c0c5af6262ad258b
nishiyu <y.nishikawa.sys22@morijyobi.ac.jp>
2022-09-15 11:07:49 +0900

    100回実行するプログラムを
    作成しました。
    ありがとうございました。

--- pra2.java
+++ pra2.java
@@ -1,9 +1,11 @@
+java2{
 public static void main(String[] args) {
         for(int i=0; i<=100; i++){
-            if(i<=100){
-                i++;
+            
+                
                 System.out.println("Hello World");
-                continue;
+                
             } 
-    }
+    
+}
 }
